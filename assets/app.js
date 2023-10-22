62513bcad32a8a1a9dea0ca090e7a646e6a8625d
Richard Song <rich@Richards-MBP.lan>
2023-10-21 22:43:22 -0700

    Updated initial app functionality to async

--- assets/app.js
+++ assets/app.js
@@ -1,27 +1,26 @@
 
-// let weatherAPIKey = "8a9b986776d2999e3580193c86a5744c"
-// let lat = "34.068623"
-// let lon = "-118.027565"
+//QuerySelectors
 
-// let baseUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${weatherAPIKey}`
 
+
+//Get Weather Data
 async function weatherApi() {
     let weatherAPIKey = "8a9b986776d2999e3580193c86a5744c"
     let lat = "34.068623"
     let lon = "-118.027565"
     let baseUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${weatherAPIKey}`
 
-    const weatherResponse = await fetch(baseUrl)
+    const response = await fetch(baseUrl)
     try {
-        if (weatherResponse.ok) {
-            const weatherData = await weatherResponse.json();
-            console.log(weatherData)
+        if (response.ok) {
+            data = (await response.json())
         } else {
-            console.log(weatherResponse.status)
+            console.log(`ERROR: ${response.status}`)
         }
-    } catch(err) {
+    } catch (err) {
         console.log(err)
     }
+    console.log(data["city"]["name"])
 }
 
 weatherApi()
