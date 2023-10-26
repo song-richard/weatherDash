5cc6d5972833738b4033401949bf5065723cc6ca
Richard Song <rich@Richards-MBP.lan>
2023-10-26 14:18:26 -0700

    Fixed base url issue while only displaying standard units. Updated to imperial

--- assets/app.js
+++ assets/app.js
@@ -8,19 +8,19 @@ async function weatherApi() {
     let weatherAPIKey = "8a9b986776d2999e3580193c86a5744c"
     let lat = "34.068623"
     let lon = "-118.027565"
-    let baseUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${weatherAPIKey}`
+    let baseUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${weatherAPIKey}&units=imperial`
 
     const response = await fetch(baseUrl)
+    console.log(response.status)
     try {
         if (response.ok) {
-            data = (await response.json())
+            console.log(await response.json())
         } else {
-            console.log(`ERROR: ${response.status}`)
+            console.log(error)
         }
-    } catch (err) {
+    } catch(err) {
         console.log(err)
     }
-    console.log(data["city"]["name"])
 }
-
 weatherApi()
+
